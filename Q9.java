9910379f722900a0499764ce79be1f59ea912be9
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-02-16 12:56:55 +0530

    Added files

--- Q9.java
+++ Q9.java
@@ -1,1 +1,17 @@
-
+enum phone {
+    Samsung(50000),Apple(70000),Xiaomi(15000),OnePlus(30000),realme(12000);
+    private int price;
+    phone(int p) {
+        price = p;
+    }
+    int getPrice() {
+        return price;
+    }
+}
+public class Q9 {
+    public static void main(String args[]){
+        System.out.println("All Smartphone prices:\n");
+        for (phone i : phone.values()) System.out.println(
+                i + " costs ₹" + i.getPrice());
+    }
+}
