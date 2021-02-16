45f50adf3d17505919908cf3ac577d0a85d17406
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-02-16 13:06:46 +0530

    Update Q11.java

--- Q11.java
+++ Q11.java
@@ -1,3 +1,5 @@
+// Q11.Create 3 subclass of bank SBI,BOI,ICICI all 4 should have method called getDetails which provide there specific details like rateofinterest etc, print details of every banks
+
 class Bank{
     int getDetails() {
         return 7;
