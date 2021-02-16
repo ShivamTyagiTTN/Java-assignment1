b8f16c4707cedeabf553b386536dcbcc6bd9e46f
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-02-16 13:04:31 +0530

    Update Q8.java

--- Q8.java
+++ Q8.java
@@ -1,3 +1,5 @@
+// Q8. Write a program to reverse a string and remove character from index 4 to index 9 from the reversed string using String Buffer
+
 public class Q8 {
     public static void main(String[] args) {
         String str = "its not a bug its a feature";
